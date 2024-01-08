4145f9f9a4fae6d78e9eaae7e04a1c5023089c9e
ub <kimub1204@gmail.com>
2024-01-08 17:04:09 +0900

    design: 상품 상세 페이지 퍼블리싱 중...

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -2,11 +2,11 @@
   <section class="productDetailWrapper">
     <img :src="importedProduct" alt="상품 이미지" class="productImg" />
     <ProductInfo
-      name="SG87W"
-      price="189000"
-      :colors="['블랙', '화이트', '그린']"
+      :productInfo="productInfo"
       :currentColor="currentColor"
-      @pickColor="handleColorPick"
+      :currentType="currentType"
+      @selectColor="updateSelectedColor"
+      @selectType="updateSelectedType"
     />
   </section>
 </template>
@@ -18,10 +18,26 @@ import product from "@/assets/images/product.png";
 
 const importedProduct = ref(product);
 const currentColor = ref("");
+const currentType = ref("");
+const productInfo = ref({
+  colors: [
+    { id: 1, name: "블랙" },
+    { id: 2, name: "화이트" },
+    { id: 3, name: "그린" },
+  ],
+  types: [
+    { id: 1, name: "SG87W" },
+    { id: 2, name: "SG88W" },
+    { id: 3, name: "SG89W" },
+  ],
+  name: "SG87W",
+  price: 189000,
+  deliveryPrice: 3000,
+});
 
-const handleColorPick = ({ color }) => {
-  currentColor.value = color;
-};
+const updateSelectedColor = ({ color }) => (currentColor.value = color);
+
+const updateSelectedType = ({ type }) => (currentType.value = type);
 </script>
 
 <style scoped>
