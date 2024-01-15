572387a0839031cbc003a187d247b6ab0c51568e
tngud100 <33285109@naver.com>
2024-01-15 11:15:43 +0900

    CICD deploy

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: [
-      { find: "#", replacement: path.resolve(__dirname, "/src/components") },
-      { find: "@", replacement: path.resolve(__dirname, "/src") },
+      { find: "#", replacement: path.resolve(__dirname, "src/components") },
+      { find: "@", replacement: path.resolve(__dirname, "src") },
     ],
   },
   server: {
