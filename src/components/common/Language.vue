3b89517e4a27282ed2c751eea9607e1b61c5ef46
tngud100 <33285109@naver.com>
2024-01-16 14:04:20 +0900

    Language Image 버그 fix

--- src/components/common/Language.vue
+++ src/components/common/Language.vue
@@ -1,37 +1,33 @@
 <template>
   <div class="languageWrapper" @click="toggleLanguage">
     <button type="button" class="world"></button>
-    <button type="button" class="language" ref="animatedElement"></button>
+    <button
+      type="button"
+      class="language"
+      :style="{
+        backgroundImage: language.isLanguage
+          ? `url(${importedKorean})`
+          : `url(${importedEnglish})`,
+      }"
+    ></button>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { animateLanguage } from "@/utils/anime";
 import { useLanguageStore } from "@/stores/useLanguageStore";
 import korean from "/korean.svg";
 import english from "@/assets/images/english.svg";
 
 const language = useLanguageStore();
-const animatedElement = ref(null);
 const importedKorean = ref(korean);
 const importedEnglish = ref(english);
-
-onMounted(() => {
-  setLanguageBackgroundImg();
-});
 
 const toggleLanguage = () => {
   animateLanguage(() => {
     language.toggleLanguage();
-    setLanguageBackgroundImg();
   });
-};
-
-const setLanguageBackgroundImg = () => {
-  animatedElement.value.style.backgroundImage = language.isLanguage
-    ? `url(${importedKorean.value})`
-    : `url(${importedEnglish.value})`;
 };
 </script>
 
