dc4c94d6ed933212c07909deb93db202305388d4
tngud100 <33285109@naver.com>
2024-06-05 19:01:49 +0900

    toolbar

--- src/components/noticeEdit/Contents.vue
+++ src/components/noticeEdit/Contents.vue
@@ -6,12 +6,26 @@
 
 <script setup>
 import { QuillEditor } from "@vueup/vue-quill";
-
+//https://quilljs.com/docs/modules/toolbar
 const toolbarOptions = [
-  ["bold", "italic", "underline", "strike"],
-  [{ size: ["small", false, "large"] }],
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+  ["link", "image", "video", "formula"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  [{ align: ["", "center", "right"] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"],
 ];
 </script>
 
